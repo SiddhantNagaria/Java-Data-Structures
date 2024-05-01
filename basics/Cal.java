3eac53aa990d1899245be49fa72d6432b2216a84
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-05-01 22:44:20 +0530

    calendar

--- basics/Cal.java
+++ basics/Cal.java
@@ -1,5 +1,5 @@
 import java.util.Calendar;
-public class Calendar {
+public class Cal {
     public static void main(String[] args) {
         Calendar cal = Calendar.getInstance();
         System.out.println("today is"+ cal.getTime());
