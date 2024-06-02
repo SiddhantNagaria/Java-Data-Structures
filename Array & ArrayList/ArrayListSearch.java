42df144c6b02ff16d79be5334ac599b9b8c3b781
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-06-02 17:19:08 +0530

    2-6-25

--- Array & ArrayList/ArrayListSearch.java
+++ Array & ArrayList/ArrayListSearch.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class ArrayListSearch {
     public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
+        ArrayListSearch arrayList = new ArrayListSearch();
         arrayList.add("1");
         arrayList.add("2");
         arrayList.add("3");
