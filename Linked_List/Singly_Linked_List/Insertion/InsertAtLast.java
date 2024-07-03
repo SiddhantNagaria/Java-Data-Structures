f0acf42558a189913923dfd8b80060a3a373619f
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-04 00:57:10 +0530

    5-7-24

---
+++ Linked_List/Singly_Linked_List/Insertion/InsertAtLast.java
@@ -0,0 +1,33 @@
+
+public class InsertAtLast {
+
+    public static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public static void main(String args[]) {
+        InsertAtBegin list = new InsertAtBegin();
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
+    }
+}
