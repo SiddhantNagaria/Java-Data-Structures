5de7727221f9600dcb41f1628492085666b0692c
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-03 21:29:35 +0530

    3-7-24

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -1,3 +1,5 @@
+package Sorting;
+
 public class Insertion_Sort {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -1,3 +1,5 @@
+package Sorting;
+
 public class Selection_Sort {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
