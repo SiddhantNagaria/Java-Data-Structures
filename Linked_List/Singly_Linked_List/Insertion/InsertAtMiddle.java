aed8a688a77c52dedc6a3d312de040e8012ad303
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-07 00:53:37 +0530

    6-7-24

---
+++ Linked_List/Singly_Linked_List/Insertion/InsertAtMiddle.java
@@ -0,0 +1,48 @@
+class InsertAtMiddle {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    public void insertAtMiddle(int idx, int data) {
+        if (idx < 1) {
+            System.out.println("Invalid index");
+        } else if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
