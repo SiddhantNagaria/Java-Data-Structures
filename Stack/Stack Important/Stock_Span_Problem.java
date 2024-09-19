5317522ab7e4d2f14b6988f2032055f501b6d373
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-09-19 23:27:39 +0530

    stock span

---
+++ Stack/Stack Important/Stock_Span_Problem.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+//Span is the maximum number of consecutive days for which
+//the stock price has been increasing.
+//price <= today's price
+
+public class Stock_Span_Problem{
+
+    public static void stockSpan(int stocks[], int span[]){
+        Stack<Integer> s = new Stack<Integer>();
+        span[0] = 1;
+        s.push(0);
+        for(int i=1;i<stocks.length;i++){
+            int currPrice = stocks[i];
+            while(!s.isEmpty() && stocks[s.peek()] <= currPrice){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i-prevHigh;
+            }
+            s.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int stocks[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+
+        for(int i = 0; i<span.length; i++){
+            System.out.print(span[i] + " ");
+        }
+    }
+}
