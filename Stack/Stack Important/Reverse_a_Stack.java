2e1d98cf2ee12f012637d69690e85c3b35900d1e
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-09-19 20:55:49 +0530

    reverse a stack

---
+++ Stack/Stack Important/Reverse_a_Stack.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class Reverse_a_Stack{
+    public static void pushAtBottom(Stack<Integer>s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printstack(Stack<Integer>s){
+        while(!s.isEmpty()){
+            System.out.print(s.pop()+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        //3,2,1
+        reverseStack(s);
+        printstack(s);
+        //1,2,3
+    }
+}
