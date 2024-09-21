d20f59a8f370cc69271d57bccc4a240b77b5b803
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-09-21 23:53:02 +0530

    next Greater element

--- Stack/Stack Important/Next_Greater_Element.java
+++ Stack/Stack Important/Next_Greater_Element.java
@@ -1,5 +1,29 @@
+
+import java.util.Stack;
+
 public class Next_Greater_Element{
     public static void main(String[] args) {
-        
+        Stack<Integer> s = new Stack<>();
+        int[] arr = {6,8,0,1,3};
+        int[] nxtGreater = new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            //1 while
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
+            }
+            //2 if
+            if(s.isEmpty()){
+                nxtGreater[i] = -1;
+            }
+            else{
+                nxtGreater[i] = arr[s.peek()];
+            }
+            //3 push
+            s.push(i);
+        }
+        for(int i=0; i < nxtGreater.length;i++){
+            System.out.print(nxtGreater[i]+" ");
+        }
+        System.out.println();
     }
 }
