c09c81a10b18e3deff5b06403f058c84dcc3d10b
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-09-28 23:03:32 +0530

    stack using dynamic arary

---
+++ Stack/Strong Implementation/Dynamic_Array_Stack.java
@@ -0,0 +1,11 @@
+public class Dynamic_Array_Stack{
+    private int[] array;
+    private int top;
+    private int size;
+    public Dynamic_Array_Stack(int initialCapacity){
+        array = new int[initialCapacity];
+        top = -1;
+        size = 0;
+    }
+
+}
