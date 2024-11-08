33722c65a3f8d2e605f88f11e190b039b6231c55
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-08 20:22:21 +0530

    sum off nodes in binary tree

---
+++ Binary Tree/Sum_of_Nodes_Binary_Tree.java
@@ -0,0 +1,34 @@
+
+public class Sum_of_Nodes_Binary_Tree {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int sumNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.data + sumNodes(root.left) + sumNodes(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println("Sum of nodes in tree is " + sumNodes(root));
+    }
+}
