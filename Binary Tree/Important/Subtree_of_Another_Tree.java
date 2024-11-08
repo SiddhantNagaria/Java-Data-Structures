6187639b1bd5675e318d4d8b461efc974699efaa
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-09 00:15:45 +0530

    folder update

---
+++ Binary Tree/Important/Subtree_of_Another_Tree.java
@@ -0,0 +1,12 @@
+public class Subtree_of_Another_Tree{
+    static class Node{
+        int data;
+        Node left, right;
+        Node(int data){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+}
