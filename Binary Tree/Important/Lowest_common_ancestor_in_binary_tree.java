b637d73b9de1fd5134f7bae2855c6c692beb36b4
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-13 11:18:01 +0530

    lca approach 2

--- Binary Tree/Important/Lowest_common_ancestor_in_binary_tree.java
+++ Binary Tree/Important/Lowest_common_ancestor_in_binary_tree.java
@@ -48,6 +48,20 @@ class Lowest_common_ancestor_in_binary_tree{
         Node lca = path1.get(i-1);
         return lca;
     }
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node left = lca(root.left, n1, n2);
+        Node right = lca(root.right, n1, n2);
+        if (right == null) {
+            return left;
+        }
+        if (left == null) {
+            return right;
+        }
+        return root;
+    }
 
     public static void main(String[] args) {
         Node root = new Node(1);
