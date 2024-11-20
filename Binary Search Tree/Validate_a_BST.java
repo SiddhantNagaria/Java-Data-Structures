e3b6c18b9983cbe44508eb6f33a6874a707b3300
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-20 15:22:59 +0530

    validate a BST

---
+++ Binary Search Tree/Validate_a_BST.java
@@ -0,0 +1,37 @@
+
+class Validate_a_BST {
+
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.data <= min.data) {
+            return false;
+        }
+        if (max != null && root.data >= max.data) {
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    public static void main(String[] args) {
+        Node root = null;
+        if (isValidBST(root, null, null)) {
+            System.out.println("valid");
+        } else {
+            System.out.println("not valid");
+        }
+    }
+}
