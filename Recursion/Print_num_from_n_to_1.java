f1d16017323e06c398a0dc46904abd53754a5f66
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-22 19:15:20 +0530

    Print_num_from_n_to_1

---
+++ Recursion/Print_num_from_n_to_1.java
@@ -0,0 +1,15 @@
+class Print_num_from_n_to_1{
+
+    public static void print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.err.println(n + " ");
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        print(n);
+    }
+}
