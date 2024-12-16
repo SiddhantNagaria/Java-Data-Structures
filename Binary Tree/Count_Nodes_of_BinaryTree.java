08d88d39aa87ba4b5a039dfa8f643786845c5d74
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-12-16 22:26:47 +0530

    size of B.T.

--- Binary Tree/Count_Nodes_of_BinaryTree.java
+++ Binary Tree/Count_Nodes_of_BinaryTree.java
@@ -1,3 +1,7 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Count_Nodes_of_BinaryTree{
     static class Node{
         int data;
@@ -9,12 +13,36 @@ public class Count_Nodes_of_BinaryTree{
             this.right = null;
         }
     }
+
+    //recursive
     public static int countNodes(Node root){
         if(root == null){
             return 0;
         }
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
+
+    //iterative
+    public int size(Node root){
+        int count =0;
+        if(root == null){
+            return 0;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node temp = q.poll();
+            count++;
+            if(temp.left!=null){
+                q.add(temp.left);
+            }
+            if(temp.right != null){
+                q.add(temp.right);
+            }
+        }
+        return count;
+    }
+    
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
