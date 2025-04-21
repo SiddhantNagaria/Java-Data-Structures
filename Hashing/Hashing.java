2454ecf744b70af3effc6b23b96c45ef5af2b767
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-21 23:10:04 +0530

    Hashmap

---
+++ Hashing/Hashing.java
@@ -0,0 +1,18 @@
+
+import java.util.HashMap;
+
+public class Hashing {
+    public static void main(String[] args) {
+        //create a hash map
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //insert key value pair
+        hm.put("India", 100);
+        hm.put("Pakistan", 200);
+        hm.put("Nepal", 300);
+        hm.put("Srilanka", 400);
+
+        
+        
+    }
+}
