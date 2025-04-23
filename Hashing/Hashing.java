9b2a81fe4a08e19a9ec536751ae965f7a6955da4
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-23 22:20:38 +0530

    contains key in hashmap

--- Hashing/Hashing.java
+++ Hashing/Hashing.java
@@ -6,15 +6,17 @@ public class Hashing {
         //create a hash map
         HashMap<String, Integer> hm = new HashMap<>();
 
-        //insert key value pair
+        //insert key value pair - O(1)
         hm.put("India", 100);
         hm.put("Pakistan", 200);
         hm.put("Nepal", 300);
         hm.put("Srilanka", 400);
 
-        //get value
+        //get value - O(1)
         System.out.println(hm.get("India"));
         
+        //cotains key - O(1)
+        System.out.print(hm.containsKey("India"));
         
         
     }
