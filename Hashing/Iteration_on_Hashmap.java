b556a99e995b14ed406030011e91266a0aae5a39
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-04-28 23:48:10 +0530

    iterate using entrySet

--- Hashing/Iteration_on_Hashmap.java
+++ Hashing/Iteration_on_Hashmap.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Iteration_on_Hashmap {
@@ -19,6 +20,11 @@ public class Iteration_on_Hashmap {
         Set<String> keys = hm.keySet();
         for (String key : keys) {
             System.out.println(key + " " + hm.get(key));
+        }
+        
+        // Way 3: Iterate using entrySet()
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
