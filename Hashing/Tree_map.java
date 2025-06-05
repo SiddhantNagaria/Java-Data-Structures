b0efd0334fefe8fc23361222826a777670174ab6
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-05 23:12:27 +0530

    tree map

---
+++ Hashing/Tree_map.java
@@ -0,0 +1,10 @@
+import java.util.TreeMap;
+
+public class Tree_map {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        tm.put("india",500);
+        tm.put("asia", 300);
+        System.out.println(tm);
+    }
+}
