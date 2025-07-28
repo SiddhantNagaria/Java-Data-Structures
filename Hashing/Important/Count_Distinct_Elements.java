82891e3e43272a24daf080ef576ebfbd7a68855c
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-07-28 20:16:44 +0530

    count distinct elements

---
+++ Hashing/Important/Count_Distinct_Elements.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class Count_Distinct_Elements {
+    public static void main(String[] args) {
+        int nums[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        System.out.println("no of distinct elements are" + set.size());
+    }
+}
