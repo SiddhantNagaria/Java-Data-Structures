b16b01d6f46b573bc567b8d07e392433c3abd4e8
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-07-28 20:08:11 +0530

    Tree set

---
+++ Hashing/Tree_Set.java
@@ -0,0 +1,13 @@
+
+import java.util.TreeSet;
+
+public class Tree_Set {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(2);
+        ts.add(1);
+        ts.add(7);
+        ts.add(4);
+        System.out.println(ts);
+    }
+}
