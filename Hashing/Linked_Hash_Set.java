c4df57eca5c58be4cb16f8d11989f7ac838be036
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-07-28 16:12:01 +0530

    linked hashset

---
+++ Hashing/Linked_Hash_Set.java
@@ -0,0 +1,13 @@
+
+import java.util.LinkedHashSet;
+
+public class Linked_Hash_Set {
+    public static void main(String[] args) {
+        LinkedHashSet<String>lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("mumbai");
+        lhs.add("bengaluru");
+        System.out.println(lhs);
+    }
+    
+}
