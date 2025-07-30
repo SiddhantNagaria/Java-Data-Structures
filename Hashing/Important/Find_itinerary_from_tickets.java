af9b05f5fb76ace31abf408f734361616bff00dc
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-07-30 17:02:00 +0530

    Find_itinerary_from_tickets

---
+++ Hashing/Important/Find_itinerary_from_tickets.java
@@ -0,0 +1,33 @@
+
+import java.util.HashMap;
+
+public class Find_itinerary_from_tickets {
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revMap = new HashMap<>();
+        for (String key : tickets.keySet()) {
+            revMap.put(tickets.get(key), key);
+        }
+        for (String key : tickets.keySet()) {
+            if (!revMap.containsKey(key)) {
+                return key; // starting point
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) { //O(n)
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+        System.out.println(start);
+        for (String key : tickets.keySet()) {
+            System.out.println(" -> " + tickets.get(start));
+            start = tickets.get(start);
+        }
+        System.out.println();
+    }
+}
